6ed91429dae4de3894bb61608e3f8f8157011c8c
anaobi <anaobii@gmail.com>
2024-04-29 15:23:45 +0100

    first build

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,7 +1,9 @@
-# HACK: from __future__ import annotations must be the first line, but conda build injects print statements. 
-#import _run_test
 from __future__ import annotations
 
 from google.protobuf.struct_pb2 import ListValue, Struct
 
 print("Tests complete")
+
+#Below was at the top
+# HACK: from __future__ import annotations must be the first line, but conda build injects print statements.
+#import _run_test
