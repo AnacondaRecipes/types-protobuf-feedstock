322d047ed231570e332308397dc4949a4bb907b3
anaobi <anaobii@gmail.com>
2024-04-29 15:31:39 +0100

    first build

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,6 @@
 #from __future__ import annotations
-from google.protobuf.struct_pb2 import ListValue, Struct
+#from google.protobuf.struct_pb2 import ListValue, Struct
+import types-protobuf
 
 print("Tests complete")
 
